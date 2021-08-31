923401512d367f2615bb3c8b69d899de65701636
sstenn <stenjansen@gmail.com>
2021-08-31 10:30:37 +0200

    Update App.vue

--- src/apps/app1/App.vue
+++ src/apps/app1/App.vue
@@ -1,8 +1,7 @@
 <template>
 	<div id="nav">
 		<router-link to="/">home</router-link> |
-		<router-link to="/about">a</router-link> |
-		<router-link to="/example">e</router-link>
+		<router-link to="/about">about</router-link> |
 	</div>
 	<router-view />
 </template>
