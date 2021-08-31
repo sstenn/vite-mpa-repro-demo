588d8d4d108ec5dfbe73d644389d79c55c71d8a8
sstenn <stenjansen@gmail.com>
2021-08-31 10:30:05 +0200

    Create Index.vue

---
+++ src/apps/app1/views/Index.vue
@@ -0,0 +1,15 @@
+<template>
+	<div>test</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Index'
+})
+</script>
+
+<style scoped>
+
+</style>
