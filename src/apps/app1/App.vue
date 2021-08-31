99e1287ea717d444e91b742e65b30e2587455a8b
sstenn <stenjansen@gmail.com>
2021-08-31 10:28:54 +0200

    Add files via upload

---
+++ src/apps/app1/App.vue
@@ -0,0 +1,15 @@
+<template>
+	<div id="nav">
+		<router-link to="/">home</router-link> |
+		<router-link to="/about">a</router-link> |
+		<router-link to="/example">e</router-link>
+	</div>
+	<router-view />
+</template>
+
+<script lang="ts">
+</script>
+
+<style>
+
+</style>
