f84bc4dd8adafc476348a26b326b15175e4739a7
sstenn <stenjansen@gmail.com>
2021-08-31 10:22:18 +0200

    Add files

---
+++ vite.config.ts
@@ -0,0 +1,31 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { resolve } from "path"
+import mpa from 'vite-plugin-mpa'
+
+export default defineConfig({
+  base: "./",
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "./src")
+    }
+  },
+  plugins: [
+    vue({
+      script: {
+        refSugar: true
+      }
+    }),
+    mpa({
+      open: 'start',
+      scanDir: 'src/apps'
+    })
+  ],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "./src/path-to-scss-variables";`
+      },
+    },
+  },
+})
